3d94bb1eff52d4cfa70b2e31a763f88cf3c77a89
David Tsuji <dts@sitecore.net>
2014-01-30 10:46:57 +1000

    added sugar (is.a, is.not, is.not.a)

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 var type = require( "./ises/type" ),
-  is = {};
+  is = {
+    a: {},
+    not: {
+      a: {}
+    }
+  };
 
-/**
- * Types
- */
-is.arguments = type( "arguments" );
-is.array = type( "array" );
-is.boolean = type( "boolean" );
-is.date = type( "date" );
-is.fn = is.func = is[ "function" ] = type( "function" );
-is.null = type( "null" );
-is.number = is.integer = type( "number" );
-is.object = type( "object" );
-is.regexp = type( "regexp" );
-is.string = type( "string" );
-is.undefined = type( "undefined" );
+var ises = {
+  "arguments": [ "arguments", type( "arguments" ) ],
+  "array": [ "array", type( "array" ) ],
+  "boolean": [ "boolean", type( "boolean" ) ],
+  "date": [ "date", type( "date" ) ],
+  "function": [ "function", "func", "fn", type( "function" ) ],
+  "null": [ "null", type( "null" ) ],
+  "number": [ "number", "integer", type( "number" ) ],
+  "object": [ "object", type( "object" ) ],
+  "regexp": [ "regexp", type( "regexp" ) ],
+  "string": [ "string", type( "string" ) ],
+  "undefined": [ "undefined", type( "undefined" ) ],
+  "empty": [ "empty", require( "./ises/empty" ) ],
+  "nullorundefined": [ "nullOrUndefined", require( "./ises/nullorundefined" ) ]
+}
 
-/**
- * Helpers
- */
-is.empty = require( "./ises/empty" );
-is.nullOrUndefined = require( "./ises/null-or-undefined" );
+Object.keys( ises ).forEach( function ( key ) {
+
+  var methods = ises[ key ].slice( 0, ises[ key ].length - 1 ),
+    fn = ises[ key ][ ises[ key ].length - 1 ];
+
+  methods.forEach( function ( methodKey ) {
+    is[ methodKey ] = is.a[ methodKey ] = fn;
+    is.not[ methodKey ] = is.not.a[ methodKey ] = function () {
+      return fn.apply( this, arguments ) ? false : true;
+    }
+  } );
+
+} );
 
 module.exports = is;
--- test/index.test.js
+++ test/index.test.js
@@ -6,55 +6,84 @@ describe( "is", function () {
   it( "should check types", function () {
 
     is.arguments( arguments ).should.be.true;
+    is.a.arguments( arguments ).should.be.true;
     is.arguments( [] ).should.be.false;
+    is.not.arguments( [] ).should.be.true;
+    is.not.a.arguments( [] ).should.be.true;
     is.arguments().should.be.false;
 
     is.array( [] ).should.be.true;
+    is.a.array( [] ).should.be.true;
     is.array( {} ).should.be.false;
     is.array().should.be.false;
 
     is.boolean( true ).should.be.true;
+    is.a.boolean( true ).should.be.true;
     is.boolean( false ).should.be.true;
     is.boolean( 1 ).should.be.false;
+    is.not.boolean( 1 ).should.be.true;
+    is.not.a.boolean( 1 ).should.be.true;
     is.boolean().should.be.false;
 
     is.date( new Date() ).should.be.true;
+    is.a.date( new Date() ).should.be.true;
     is.date( "29 aug 1997" ).should.be.false;
     is.date().should.be.false;
 
     is.fn( function () {} ).should.be.true;
+    is.a.fn( function () {} ).should.be.true;
     is.fn( Object ).should.be.true;
     is.fn( describe ).should.be.true;
     is.fn( this ).should.be.false;
+    is.not.fn( this ).should.be.true;
+    is.not.a.fn( this ).should.be.true;
     is.fn().should.be.false;
 
     is.null( null ).should.be.true;
+    is.a.null( null ).should.be.true;
     is.null( undefined ).should.be.false;
+    is.not.null( undefined ).should.be.true;
+    is.not.a.null( undefined ).should.be.true;
     is.null( 0 ).should.be.false;
     is.null( -1 ).should.be.false;
     is.null( false ).should.be.false;
     is.null().should.be.false;
 
     is.number( 1 ).should.be.true;
+    is.a.number( 1 ).should.be.true;
     is.number( "1" ).should.be.false;
+    is.not.number( "1" ).should.be.true;
+    is.not.a.number( "1" ).should.be.true;
     is.number().should.be.false;
 
     is.object( {} ).should.be.true;
+    is.a.object( {} ).should.be.true;
     is.object( this ).should.be.true;
     is.object().should.be.false;
+    is.not.object().should.be.true;
+    is.not.a.object().should.be.true;
 
     is.regexp( new RegExp() ).should.be.true;
+    is.a.regexp( new RegExp() ).should.be.true;
     is.regexp( /^/ ).should.be.true;
     is.regexp().should.be.false;
+    is.not.regexp().should.be.true;
+    is.not.a.regexp().should.be.true;
 
     is.string( "a" ).should.be.true;
+    is.a.string( "a" ).should.be.true;
     is.string( "" ).should.be.true;
     is.string( true ).should.be.false;
+    is.not.string( true ).should.be.true;
+    is.not.a.string( true ).should.be.true;
     is.string().should.be.false;
 
     is.undefined( undefined ).should.be.true;
+    is.a.undefined( undefined ).should.be.true;
     is.undefined().should.be.true;
     is.undefined( "undefined" ).should.be.false;
+    is.not.undefined( "undefined" ).should.be.true;
+    is.not.a.undefined( "undefined" ).should.be.true;
     is.undefined( null ).should.be.false;
     is.undefined( false ).should.be.false;
     is.undefined( 0 ).should.be.false;
